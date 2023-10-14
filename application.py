0684134873f13a3dd1a0772f9993aa8bdc9810b7
Brayancito2503 <brayan.potosme03@gmail.com>
2023-10-13 20:23:31 -0600

    mas estilos al incio

--- application.py
+++ application.py
@@ -1,20 +1,35 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, send, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
-app.run(debug=True)
+
 MAX_MESSAGES = 5
 CHANNELS = {}
+USUARIOS = {}
 
 @app.route("/")
 def canales():
     return render_template("canales.html")
 
 # SOCKETS
-@socketio.on('connect')
+@socketio.on("connect")
 def conecciones():
-    return 'cliente conectado'
+    print("cliente conectado")
+
+@socketio.on("ingreso_usuario")
+def usuarios(usuarios):
+    print(f'Usuario ingresado: {usuarios}')
+    USUARIOS[usuarios] = request.sid
+
+@socketio.on("nuevo_mensaje")
+def mensajes(mensajes):
+    print(f"mensaje {mensajes}")
+    usuarios = None
+    for usuario in USUARIOS:
+        if(USUARIOS[usuario] == request.sid):
+            usuarios = usuario
+    emit("chat", {"mensajes": mensajes, "usuario": usuarios} , broadcast = True)
